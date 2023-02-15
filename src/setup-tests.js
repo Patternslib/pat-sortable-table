12d98efe1836c90c59b570380f45c1a2641953eb
Johannes Raggam <thetetet@gmail.com>
2023-02-15 20:05:48 +0100

    maint: Provide jQuery to datatables, as this is not done by @patternslib/dev anymore.

--- src/setup-tests.js
+++ src/setup-tests.js
@@ -1,2 +1,6 @@
+// provide jquery
+import jquery from "jquery";
+global.$ = global.jQuery = jquery;
+
 // Attach datatables to jQuery, as in tests it is not done by just importing.
 window.dt = require("datatables.net")();
